f07bbe4167b764c7635cde74a304807f3dc2b8d4
Yuliia Nivina <y.nivina@gmail.com>
2023-06-22 01:13:25 +0300

    Add HW-08 fix

--- src/components/ContactsPage/ContactsPage.jsx
+++ src/components/ContactsPage/ContactsPage.jsx
@@ -1,16 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getIsLoading, getError } from 'redux/contacts/selectors';
+import { fetchContacts } from 'redux/contacts/operations';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 import css from './ContactsPage.module.css';
 
-const ContactsPage = () => (
-  <div className={css.contacts__box}>
-    <h1>Phonebook</h1>
-    <ContactForm />
-    <h2>Contacts</h2>
-    <Filter />
-    {<ContactList />}
-  </div>
-);
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.contacts__box}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {<ContactList />}
+      {isLoading && !error && <b>Request in progress...</b>}
+    </div>
+  );
+};
 
 export default ContactsPage;
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,24 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getIsLoading, getError } from 'redux/contacts/selectors';
-import { fetchContacts } from 'redux/contacts/operations';
 import ContactsPage from 'components/ContactsPage/ContactsPage';
 
-const Contacts = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  return (
-    <>
+const Contacts = () => (
       <ContactsPage />
-      {isLoading && !error && <b>Request in progress...</b>}
-    </>
   );
-};
 
 export default Contacts;
